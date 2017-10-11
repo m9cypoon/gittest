7d7f0bf0b986d116ee7ab9337768b661ce9d7120
Michael Poon <80378554@qq.com>
2017-10-11 23:26:26 +0800

    modify HelloWorld class and recompiled

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,11 @@ class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Git");
+
+		System.out.println(add(3, 4));
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
 	}
 }
